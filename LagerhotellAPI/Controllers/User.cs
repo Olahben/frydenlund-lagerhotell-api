db258634ed4f3fc93f1400d343e3e7e47552169b
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-11-10 15:10:34 +0100

    Create API (Working on swagger)

--- LagerhotellAPI/Controllers/User.cs
+++ LagerhotellAPI/Controllers/User.cs
@@ -1,1 +1,33 @@
 ﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+[ApiController]
+[Route("user")]
+public class UserController : ControllerBase
+{
+    private readonly string filePath = @"C:\Users\ohage\SKOLE\Programmering\Lagerhotell\wwwroot\Data\users.json";
+    [Route("adduser")]
+    [HttpPost]
+    public IActionResult AddUser([FromBody] Dictionary<string, object> data)
+    {
+        List<object> _data = new List<object>() { data };
+
+        string existingJson = System.IO.File.ReadAllText(filePath);
+        JObject jsonObject = JsonConvert.DeserializeObject<JObject>(existingJson);
+
+        JArray usersArray = jsonObject.GetValue("users") as JArray;
+        usersArray.Add(JToken.FromObject(data));
+
+        string updatedJson = jsonObject.ToString();
+
+        System.IO.File.WriteAllText(filePath, updatedJson);
+
+
+        return Ok();
+    }
+}
