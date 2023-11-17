1b00e91831160588d5a4fc6cc02efa952651d4a3
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-11-17 15:36:24 +0100

    Create a method that checks for the previous registration of a phone number

--- LagerhotellAPI/Controllers/User.cs
+++ LagerhotellAPI/Controllers/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 [ApiController]
 [Route("user")]
@@ -23,6 +24,33 @@ public class UserController : ControllerBase
 
         System.IO.File.WriteAllText(filePath, updatedJson);
 
+
+        return Ok();
+    }
+
+    [Route("is-phone-number-registered-registration")]
+    [HttpPost]
+    public IActionResult CheckPhoneNumberExistence([FromBody] string phoneNumber)
+    {
+        // reads text from JSON and checks if phone number is there
+        // returns true if it registered and false if not
+        string existingJson = System.IO.File.ReadAllText(filePath);
+        // Parse the JSON data
+        JObject jsonObject = JObject.Parse(existingJson);
+
+        // Access the "users" array
+        JArray usersArray = (JArray)jsonObject["users"];
+        Console.WriteLine(usersArray);
+
+        foreach (var item in usersArray)
+        {
+            Console.WriteLine(item["phoneNumber"]);
+            Console.WriteLine(phoneNumber);
+            if (phoneNumber == item["phoneNumber"].ToString())
+            {
+                return BadRequest("False");
+            }
+        }
 
         return Ok();
     }
