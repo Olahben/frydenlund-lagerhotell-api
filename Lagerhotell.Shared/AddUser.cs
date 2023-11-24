0d3e134c4af6127547c36b1f1e6bfae235cf1b18
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-11-24 11:48:34 +0100

    Wirte all properties

--- Lagerhotell.Shared/AddUser.cs
+++ Lagerhotell.Shared/AddUser.cs
@@ -4,5 +4,8 @@
     {
 
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
     }
 }
