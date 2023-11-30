f4b58ed3b45eb4dcf93903daab273152379732b7
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-11-30 12:57:00 +0100

    Create AddUserRequest class

--- Lagerhotell.Shared/AddUser.cs
+++ Lagerhotell.Shared/AddUser.cs
@@ -1,11 +1,32 @@
-﻿namespace LagerhotellAPI.Models
+﻿using System.Data;
+
+namespace LagerhotellAPI.Models
 {
     public class AddUserRequest
     {
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private string phoneNumber { get; set; }
+        private string birthDate { get; set; }
+        private string password { get; set; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+
+        public string GetFirstName() => firstName;
+        public string GetLastName() => lastName;
+        public string GetPhoneNumber() => phoneNumber;
+        public string GetBirthDate() => birthDate;
+        public string GetPassword() => password;
+        static public AddUserRequest AddUserRequestFunc(string FirstName, string LastName, string PhoneNumber, string BirthDate, string Password)
+        {
+            var addUserRequest = new AddUserRequest
+            {
+                firstName = FirstName,
+                lastName = LastName,
+                phoneNumber = PhoneNumber,
+                birthDate = BirthDate,
+                password = Password
+            };
+            return addUserRequest;
+        }
     }
 }
