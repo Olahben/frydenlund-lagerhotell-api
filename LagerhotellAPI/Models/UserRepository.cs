28aaa01f1431dd74eaa46923c815680a0f74d124
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-01 14:57:34 +0100

    Create UserRepos with basic methods

---
+++ LagerhotellAPI/Models/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace LagerhotellAPI.Models
+{
+    public class UserRepository
+    {
+        private List<User> _users;
+        private readonly string _filePath = @"C:\Users\ohage\SKOLE\Programmering\Lagerhotell\wwwroot\Data\users.json";
+        public List<User> Users { get {
+                if (_users == null)
+                {
+                    Load();
+                }
+                return _users;
+            }
+        }
+
+        public string Add(User user)
+        {
+            user.Id = Guid.NewGuid().ToString();
+            Users.Add(user);
+            // Ensure JSON is saved
+            return user.Id;
+            
+        }
+
+        public User? Get(string phoneNumber)
+        {
+            return Users.Where(_ => _.PhoneNumber == phoneNumber).SingleOrDefault();
+        }
+
+        // private void save
+        private void Load()
+        {
+            // Check if JSON is read
+            string existingJson = System.IO.File.ReadAllText(_filePath);
+            _users = JsonConvert.DeserializeObject<List<User>>(existingJson);
+        }
+    }
+}
