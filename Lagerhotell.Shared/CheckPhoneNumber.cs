9147c2ebf70d78b58ae293457f945e85e03b7852
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-01 14:56:13 +0100

    Add response

---
+++ Lagerhotell.Shared/CheckPhoneNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerhotellAPI.Models
+{
+    public class CheckPhoneNumberRequest
+    {
+        public string PhoneNumber { get; set;}
+    }
+    public class CheckPhoneNumberResponse {  public bool PhoneNumberExistence { get; set;} }
+}
