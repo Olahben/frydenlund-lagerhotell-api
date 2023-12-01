542bd316c187619c465bcd096c83d63f0b610c78
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-01 14:57:08 +0100

    Update prop names

--- LagerhotellAPI/Models/User.cs
+++ LagerhotellAPI/Models/User.cs
@@ -2,5 +2,19 @@
 {
     public class User
     {
+        public User(string firstName, string lastName, string phoneNumber, string birthDate, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
+            Password = password;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string PhoneNumber { get; set; }
+        public string BirthDate { get; set;}
+        public string Password { get; set;}
+        public string Id { get; set; }
     }
 }
