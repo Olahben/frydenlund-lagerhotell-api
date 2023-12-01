d1bd153f83de3ab1d77d910f3e1eebc2a4b85e1b
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-01 13:08:05 +0100

    Refactor operations to happen in Models

---
+++ LagerhotellAPI/Controllers/Users.cs
@@ -0,0 +1,35 @@
+﻿using LagerhotellAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+[ApiController]
+[Route("users")]
+public class UsersController : ControllerBase
+{
+    private readonly UserRepository _userRepository = new UserRepository();
+    [Route("adduser")]
+    [HttpPost]
+    public IActionResult AddUser([FromBody] AddUserRequest request)
+    {
+        _userRepository.Add(new User(
+            request.FirstName,
+            request.LastName,
+            request.PhoneNumber,
+            request.BirthDate,
+            request.Password));
+
+
+        return Ok();
+    }
+
+    [Route("is-phone-number-registered-registration")]
+    [HttpPost]
+    public IActionResult CheckPhoneNumberExistence([FromBody] CheckPhoneNumberRequest request)
+    {
+        var user = _userRepository.Get(request.PhoneNumber);
+        // logic to check if it exists
+        return Ok(new CheckPhoneNumberResponse { PhoneNumberExistence = true});
+    }
+}
