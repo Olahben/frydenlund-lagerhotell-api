d420b69b7bb696df25945400f519a875cfebb7f1
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-12 15:02:57 +0100

    Add base class

--- Lagerhotell.Shared/CheckPhoneNumber.cs
+++ Lagerhotell.Shared/CheckPhoneNumber.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LagerhotellAPI.Models
 {
-    public class CheckPhoneNumberRequest
+    public class CheckPhoneNumber
     {
-        public string PhoneNumber { get; set;}
+        public class CheckPhoneNumberRequest
+        {
+            public string PhoneNumber { get; set; }
+        }
+
+        public class CheckPhoneNumberResponse { public bool PhoneNumberExistence { get; set; } }
     }
-    public class CheckPhoneNumberResponse {  public bool PhoneNumberExistence { get; set;} }
 }
