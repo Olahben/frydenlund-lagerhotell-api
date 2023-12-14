e5122dd867a96407f0c5e7e7cc6e4dbcb4609890
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-14 10:48:55 +0100

    Add request and response

---
+++ Lagerhotell.Shared/CheckPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerhotell.Shared
+{
+    public class CheckPassword
+    {
+        public class CheckPasswordRequest
+        {
+            public string PhoneNumber { get; set; }
+        }
+
+        public class CheckPasswordResponse
+        {
+            public string Password { get; set; }
+        }
+    }
+}
