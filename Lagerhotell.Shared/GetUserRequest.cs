23d69d60a8caef7bf4cb1233a1f7bf5dd6209a01
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-15 10:38:09 +0100

    Make GetUser response and requests

---
+++ Lagerhotell.Shared/GetUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerhotell.Models
+{
+        public class GetUserRequest
+        {
+            public string userId { get; set; }
+        }
+    
+}
---
+++ LagerhotellAPI/Models/GetUserResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace LagerhotellAPI.Models
+{
+    public class GetUserResponse
+    {
+        public User user {  get; set; }
+    }
+}
