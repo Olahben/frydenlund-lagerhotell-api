3ebe3cc1306817fbd725947aba6483bd7ea1daba
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-15 10:45:18 +0100

    Use correct namespace

--- Lagerhotell.Shared/GetUserRequest.cs
+++ Lagerhotell.Shared/GetUserRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lagerhotell.Models
+namespace LagerhotellAPI.Models
 {
         public class GetUserRequest
         {
