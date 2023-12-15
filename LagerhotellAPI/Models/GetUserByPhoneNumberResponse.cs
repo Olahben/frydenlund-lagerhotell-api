20b9ea60c126e075d58b2f88b4f7ea92b30b5238
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-15 14:18:01 +0100

    Create GetUserByPhoneNumberResponse

---
+++ LagerhotellAPI/Models/GetUserByPhoneNumberResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace LagerhotellAPI.Models
+{
+    public class GetUserByPhoneNumberResponse
+    {
+        public User User { get; set; }
+
+        public User GetUserBtPhoneNumberResponseFunc(string firstName, string lastName, string phoneNumber, string birthDate, string password, string id)
+        {
+            User.FirstName = firstName;
+            User.LastName = lastName;
+            User.PhoneNumber = phoneNumber;
+            User.BirthDate = birthDate;
+            User.Password = password;
+            User.Id = id;
+            return User;
+        }
+    }
+}
