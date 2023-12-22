989865ce17eaee2ac0f3d2f64bb5ade4bca45253
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-22 13:22:40 +0100

    Fix structure

--- LagerhotellAPI/Models/User.cs
+++ LagerhotellAPI/Models/User.cs
@@ -4,23 +4,23 @@
     {
         public User(string id, string firstName, string lastName, string phoneNumber, string birthDate, string password)
         {
+            Id = id;
             FirstName = firstName;
             LastName = lastName;
             PhoneNumber = phoneNumber;
             BirthDate = birthDate;
             Password = password;
-            Id = id;
         }
         public User()
         {
 
         }
 
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string BirthDate { get; set; }
         public string Password { get; set; }
-        public string Id { get; set; }
     }
 }
