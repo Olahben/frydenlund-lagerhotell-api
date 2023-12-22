67e3b7761bb83c47f5a0ea83063d4cd7ce0de737
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-22 12:19:18 +0100

    Make copy

---
+++ Lagerhotell.Shared/User.cs
@@ -0,0 +1,26 @@
+﻿namespace LagerhotellAPI.Models
+{
+    public class User
+    {
+        public User(string id, string firstName, string lastName, string phoneNumber, string birthDate, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
+            Password = password;
+            Id = id;
+        }
+        public User()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BirthDate { get; set; }
+        public string Password { get; set; }
+        public string Id { get; set; }
+    }
+}
