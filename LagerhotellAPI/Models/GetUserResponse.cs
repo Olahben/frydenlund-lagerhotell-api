1d2513f6dc61f3bd11a88a80ab215f0456c02cba
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2023-12-22 12:57:10 +0100

    Return User correctly

--- LagerhotellAPI/Models/GetUserResponse.cs
+++ LagerhotellAPI/Models/GetUserResponse.cs
@@ -2,15 +2,11 @@
 {
     public class GetUserResponse
     {
-        public User User {  get; set; }
+        public User User { get; set; }
 
-        public User GetUserResponseFunc(string firstName, string lastName, string phoneNumber, string birthDate, string password, string id) {
-            User.FirstName = firstName;
-            User.LastName = lastName;
-            User.PhoneNumber = phoneNumber;
-            User.BirthDate = birthDate;
-            User.Password = password;
-            User.Id = id;
+        public User GetUserResponseFunc(string id, string firstName, string lastName, string phoneNumber, string birthDate, string password)
+        {
+            User = new User(id, firstName, lastName, phoneNumber, birthDate, password);
 
             return User;
         }
