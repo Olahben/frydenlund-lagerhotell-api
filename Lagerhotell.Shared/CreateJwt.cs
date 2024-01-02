e76a5eb5f04d5ec4111af1f89143e850292d20f5
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-02 15:40:34 +0100

    Make separate request models for API and service

--- Lagerhotell.Shared/CreateJwt.cs
+++ Lagerhotell.Shared/CreateJwt.cs
@@ -2,9 +2,14 @@
 {
     public class CreateJwt
     {
-        public class CreateJwtRequest
+        public class CreateJwtRequestService
         {
             public string PhoneNumber { get; set; }
+        }
+
+        public class CreateJwtRequest
+        {
+            public string Id { get; set; }
         }
 
         public class CreateJwtResponse
