2ed06d3c54dc00c4219712ec634d73450568f060
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-18 10:54:28 +0100

    Get issuer, audience from appsettings.json

--- LagerhotellAPI/Services/TokenService.cs
+++ LagerhotellAPI/Services/TokenService.cs
@@ -2,40 +2,38 @@
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace LagerhotellAPI.Services
 {
     public class TokenService
     {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public Jwt CreateJwt(string id, string phoneNumber)
         {
-            // bitSecret should be fixed
-            string bitSecret;
-            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
-            {
-                var secretKeyByteArray = new byte[32];
-                rng.GetBytes(secretKeyByteArray);
-                bitSecret = Convert.ToBase64String(secretKeyByteArray);
-            }
-            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(bitSecret));
+            var secretKeyString = _configuration["Jwt:Key"];
+            Console.WriteLine(secretKeyString);
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKeyString));
             var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
-            var tokeOptions = new JwtSecurityToken(
-                issuer: "https://localhost:7272",
-                audience: "https://localhost:5001",
-                claims: new List<Claim>()
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: new List<Claim>
                 {
-            new Claim(ClaimTypes.Sid, id),
-            new Claim(ClaimTypes.MobilePhone, phoneNumber)
-
+                    new Claim(ClaimTypes.Sid, id),
+                    new Claim(ClaimTypes.MobilePhone, phoneNumber)
                 },
                 expires: DateTime.Now.AddMinutes(30),
                 signingCredentials: signinCredentials
             );
 
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
             return new Jwt { Token = tokenString };
         }
     }
