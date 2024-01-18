5f6d11743a9c040acb4fae57fb8d1beaa53a1981
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-18 13:15:50 +0100

    Move users controller into a named namespace

--- LagerhotellAPI/Controllers/Users.cs
+++ LagerhotellAPI/Controllers/Users.cs
@@ -3,94 +3,99 @@ using LagerhotellAPI.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-[ApiController]
-[Route("users")]
-public class UsersController : ControllerBase
+
+
+namespace Controllers
 {
-    private readonly UserRepository _userRepository = new UserRepository();
-    private readonly GetUserResponse _getuserResponse = new GetUserResponse();
-    private readonly GetUserByPhoneNumberResponse _getUserByPhoneNumberResponse = new GetUserByPhoneNumberResponse();
-    private readonly TokenService _tokenService;
-
-    public UsersController(TokenService tokenService)
+    [ApiController]
+    [Route("users")]
+    public class UsersController : ControllerBase
     {
-        _tokenService = tokenService;
-    }
+        private readonly UserRepository _userRepository = new UserRepository();
+        private readonly GetUserResponse _getuserResponse = new GetUserResponse();
+        private readonly GetUserByPhoneNumberResponse _getUserByPhoneNumberResponse = new GetUserByPhoneNumberResponse();
+        private readonly TokenService _tokenService;
 
-    [Route("is-phone-number-registered-registration")]
-    [HttpPost]
-    public IActionResult CheckPhoneNumberExistence([FromBody] CheckPhoneNumber.CheckPhoneNumberRequest request)
-    {
-        // user is null if not found
-        var user = _userRepository.Get(request.PhoneNumber);
-        // should return phone number response
-        if (user == null)
+        public UsersController(TokenService tokenService)
         {
-            return Conflict(new CheckPhoneNumber.CheckPhoneNumberResponse { PhoneNumberExistence = true });
-        }
-        else
-        {
-            return Ok(new CheckPhoneNumber.CheckPhoneNumberResponse { PhoneNumberExistence = false });
-        }
-    }
-
-    [Route("add-user")]
-    [HttpPost]
-    public IActionResult AddUser([FromBody] AddUserRequest request)
-    {
-        var user = _userRepository.Get(request.PhoneNumber);
-        if (user != null)
-        {
-
-            return Conflict(new CheckPhoneNumber.CheckPhoneNumberResponse { PhoneNumberExistence = true });
+            _tokenService = tokenService;
         }
 
-        user = _userRepository.Add(
-         request.FirstName,
-         request.LastName,
-         request.PhoneNumber,
-         request.BirthDate,
-         request.Password);
-
-        return Ok(new AddUserResponse { UserId = user.Id });
-    }
-    [Route("check-password")]
-    [HttpPost]
-    public IActionResult CheckPassword([FromBody] CheckPassword.CheckPasswordRequest request)
-    {
-        // Hent ut passordet fra brukeren, ved hjelp av mobilnummeret
-        // Lag en ny metode paa userRepository som sjekker om passordene er like
-        // den returnerer en bool
-        // Hvis passordene ikke er like, returner DeclineAccess eller noe saant
-        // Hvis passordene er like, returner JWT
-        // Hvis passord er feil, returner tom JWT
-        // Get user
-        User? user = _userRepository.Get(request.PhoneNumber);
-        if (_userRepository.DoPasswordsMatch(request.Password, user.Password))
+        [Route("is-phone-number-registered-registration")]
+        [HttpPost]
+        public IActionResult CheckPhoneNumberExistence([FromBody] CheckPhoneNumber.CheckPhoneNumberRequest request)
         {
-            Jwt jwt = _tokenService.CreateJwt(user.Id, user.PhoneNumber);
-            return Ok(new CheckPassword.CheckPasswordResponse { Token = jwt.Token });
+            // user is null if not found
+            var user = _userRepository.Get(request.PhoneNumber);
+            // should return phone number response
+            if (user == null)
+            {
+                return Conflict(new CheckPhoneNumber.CheckPhoneNumberResponse { PhoneNumberExistence = true });
+            }
+            else
+            {
+                return Ok(new CheckPhoneNumber.CheckPhoneNumberResponse { PhoneNumberExistence = false });
+            }
         }
 
-        return Unauthorized();
-    }
+        [Route("add-user")]
+        [HttpPost]
+        public IActionResult AddUser([FromBody] AddUserRequest request)
+        {
+            var user = _userRepository.Get(request.PhoneNumber);
+            if (user != null)
+            {
 
-    [Authorize]
-    [Route("get-user")]
-    [HttpPost]
-    public IActionResult GetUser([FromBody] LagerhotellAPI.Models.GetUserRequest request)
-    {
-        User? user = _userRepository.GetUserById(request.UserId);
-        return Ok(_getuserResponse.GetUserResponseFunc(user.Id, user.FirstName, user.LastName, user.PhoneNumber, user.BirthDate, user.Password));
-    }
+                return Conflict(new CheckPhoneNumber.CheckPhoneNumberResponse { PhoneNumberExistence = true });
+            }
 
-    [Authorize]
-    [Route("get-user-by-phone-number")]
-    [HttpPost]
-    public IActionResult GetUserByPhoneNumber([FromBody] GetUserByPhoneNumberRequest request)
-    {
-        var user = _userRepository.Get(request.PhoneNumber);
-        return Ok(_getUserByPhoneNumberResponse.GetUserByPhoneNumberResponseFunc(user.Id, user.FirstName, user.LastName, user.PhoneNumber, user.BirthDate, user.Password));
+            user = _userRepository.Add(
+             request.FirstName,
+             request.LastName,
+             request.PhoneNumber,
+             request.BirthDate,
+             request.Password);
 
+            return Ok(new AddUserResponse { UserId = user.Id });
+        }
+        [Route("check-password")]
+        [HttpPost]
+        public IActionResult CheckPassword([FromBody] CheckPassword.CheckPasswordRequest request)
+        {
+            // Hent ut passordet fra brukeren, ved hjelp av mobilnummeret
+            // Lag en ny metode paa userRepository som sjekker om passordene er like
+            // den returnerer en bool
+            // Hvis passordene ikke er like, returner DeclineAccess eller noe saant
+            // Hvis passordene er like, returner JWT
+            // Hvis passord er feil, returner tom JWT
+            // Get user
+            User? user = _userRepository.Get(request.PhoneNumber);
+            if (_userRepository.DoPasswordsMatch(request.Password, user.Password))
+            {
+                Jwt jwt = _tokenService.CreateJwt(user.Id, user.PhoneNumber);
+                return Ok(new CheckPassword.CheckPasswordResponse { Token = jwt.Token });
+            }
+
+            return Unauthorized();
+        }
+
+        [Authorize]
+        [Route("get-user")]
+        [HttpPost]
+        public IActionResult GetUser([FromBody] LagerhotellAPI.Models.GetUserRequest request)
+        {
+            User? user = _userRepository.GetUserById(request.UserId);
+            return Ok(_getuserResponse.GetUserResponseFunc(user.Id, user.FirstName, user.LastName, user.PhoneNumber, user.BirthDate, user.Password));
+        }
+
+        [Authorize]
+        [Route("get-user-by-phone-number")]
+        [HttpPost]
+        public IActionResult GetUserByPhoneNumber([FromBody] GetUserByPhoneNumberRequest request)
+        {
+            var user = _userRepository.Get(request.PhoneNumber);
+            return Ok(_getUserByPhoneNumberResponse.GetUserByPhoneNumberResponseFunc(user.Id, user.FirstName, user.LastName, user.PhoneNumber, user.BirthDate, user.Password));
+
+        }
     }
 }
