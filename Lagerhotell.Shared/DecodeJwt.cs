80e4091fbeb6671aacc2b2fa90047ef59b4c822c
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-19 13:05:49 +0100

    Create

---
+++ Lagerhotell.Shared/DecodeJwt.cs
@@ -0,0 +1,14 @@
+﻿namespace LagerhotellAPI.Models
+{
+    public class DecodeJwt
+    {
+        public class DecodeJwtResponse
+        {
+            public string PhoneNumber { get; set; }
+        }
+        public class DecodeJwtRequest
+        {
+            public string Token { get; set; }
+        }
+    }
+}
