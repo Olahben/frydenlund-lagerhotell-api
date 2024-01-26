44d9915d42b0dbf0e958cb927074f1fee32b2bec
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-26 13:14:26 +0100

    Add string Token to response class

--- Lagerhotell.Shared/AddUser.cs
+++ Lagerhotell.Shared/AddUser.cs
@@ -1,38 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Data;
-using System.Reflection.Metadata.Ecma335;
 
 namespace LagerhotellAPI.Models
 {
     public class AddUserRequest
     {
-        [Required]
-        public string FirstName { get; set; }
-        [Required]
-        public string LastName { get; set; }
-        [Required]
-        public string PhoneNumber { get; set; }
-        [Required]
-        public string BirthDate { get; set; }
-        [Required]
-        public string Password { get; set; }
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+        [Required] public string PhoneNumber { get; set; }
+        [Required] public string BirthDate { get; set; }
 
-        static public AddUserRequest AddUserRequestFunc(string firstName, string lastName, string phoneNumber, string birthDate, string password)
-        {
-            var addUserRequest = new AddUserRequest
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                PhoneNumber = phoneNumber,
-                BirthDate = birthDate,
-                Password = password
-            };
-            return addUserRequest;
-        }
+        [Required] public string Address { get; set; }
+
+        [Required] public string PostalCode { get; set; }
+
+        [Required] public string City { get; set; }
+        [Required] public string Password { get; set; }
 
     }
-    public class AddUserResponse 
-    { 
-        public string UserId { get; set; } 
+    public class AddUserResponse
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
     }
 }
