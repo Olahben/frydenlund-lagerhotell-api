2e3e34971c301fce6371479ab79ea153519cbf39
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-31 09:53:47 +0100

    Create

---
+++ LagerhotellAPI/Models/MongoDbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace LagerhotellAPI.Models
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string UsersCollectionName { get; set; }
+        // Add other MongoDB related settings here if needed
+    }
+}
