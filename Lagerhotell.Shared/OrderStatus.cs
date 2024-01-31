733cbfd3388e127ecd2b8f9b21cfe3dc20c816e5
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-31 14:41:29 +0100

    Create

---
+++ Lagerhotell.Shared/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace LagerhotellAPI.Models
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Confirmed,
+        Cancelled,
+        Completed
+    }
+}
---
+++ Lagerhotell.Shared/StorageUnit.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LagerhotellAPI.Models
+{
+    public class StorageUnit
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        public required double Size { get; set; }
+
+        public required bool Temperated { get; set; }
+    }
+}
