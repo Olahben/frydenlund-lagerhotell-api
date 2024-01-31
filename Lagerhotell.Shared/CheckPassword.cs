2f1052eba224e79b81b3daf5ccdcba627b13deac
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-31 10:20:04 +0100

    Fix name

--- Lagerhotell.Shared/CheckPassword.cs
+++ Lagerhotell.Shared/CheckPassword.cs
@@ -1,14 +1,14 @@
 ﻿namespace LagerhotellAPI.Models
 {
-    public class CheckPassword
+    public class Login
     {
-        public class CheckPasswordRequest
+        public class LoginRequest
         {
             public required string PhoneNumber { get; set; }
             public required string Password { get; set; }
         }
 
-        public class CheckPasswordResponse
+        public class LoginResponse
         {
             public string Token { get; set; }
         }
