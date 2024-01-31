fb02222d26db1cbcfcbd7998d8393ff9fbf6c537
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-31 10:20:40 +0100

    Add new constructor, add BSONId, and ObjectId to Id field

--- Lagerhotell.Shared/User.cs
+++ Lagerhotell.Shared/User.cs
@@ -1,5 +1,9 @@
-﻿namespace LagerhotellAPI.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LagerhotellAPI.Models
 {
+    [BsonIgnoreExtraElements]
     public class User
     {
         public User(string id, string firstName, string lastName, string phoneNumber, string birthDate, string address, string postalCode, string city, string password)
@@ -14,11 +18,25 @@
             City = city;
             Password = password;
         }
+
+        public User(string firstName, string lastName, string phoneNumber, string birthDate, string address, string postalCode, string city, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
+            Address = address;
+            PostalCode = postalCode;
+            City = city;
+            Password = password;
+        }
         public User()
         {
 
         }
 
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
