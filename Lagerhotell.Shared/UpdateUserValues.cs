2bd98291eb8a896a6622685bafc4efd479ab858d
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-01-31 10:21:20 +0100

    Create

---
+++ Lagerhotell.Shared/UpdateUserValues.cs
@@ -0,0 +1,23 @@
+﻿namespace LagerhotellAPI.Models
+{
+
+    public class UpdateUserValuesRequest
+    {
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string PhoneNumber { get; set; }
+
+        public required string BirthDate { get; set; }
+        public required string Password { get; set; }
+        public required string Address { get; set; }
+        public required string PostalCode { get; set; }
+        public required string City { get; set; }
+    }
+
+    public class UpdateUserValuesResponse
+    {
+        public required bool Success { get; set; }
+    }
+}
