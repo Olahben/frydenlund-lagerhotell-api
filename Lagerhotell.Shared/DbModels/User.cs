775eba9f8aa679827047c409a894b7a44e419f33
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-02 14:57:36 +0100

    Create user model for db

---
+++ Lagerhotell.Shared/DbModels/User.cs
@@ -0,0 +1,26 @@
+﻿
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LagerhotellAPI.DbModels;
+
+[BsonIgnoreExtraElements]
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
+
+    // Should be generated upon registation
+    public required string UserId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string BirthDate { get; set; }
+
+    public required string Address { get; set; }
+    public required string PostalCode { get; set; }
+    public required string City { get; set; }
+    public required string Password { get; set; }
+}
