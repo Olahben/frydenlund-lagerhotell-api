2117a3618e635e56578d1e871555bae91d3b798c
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-05 12:51:54 +0100

    Implement correct use of domain and database models

--- LagerhotellAPI/Services/OrderService.cs
+++ LagerhotellAPI/Services/OrderService.cs
@@ -5,16 +5,17 @@ namespace LagerhotellAPI.Services
 {
     public class OrderService
     {
-        private readonly IMongoCollection<Order> _orders;
+        private readonly IMongoCollection<LagerhotellAPI.Models.DbModels.Order> _orders;
         public OrderService(MongoDbSettings settings)
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase("Lagerhotell");
-            _orders = database.GetCollection<Order>("Orders");
+            _orders = database.GetCollection<LagerhotellAPI.Models.DbModels.Order>("Orders");
         }
         public async Task AddOrder(Order order)
         {
-            await _orders.InsertOneAsync(order);
+            LagerhotellAPI.Models.DbModels.Order dbOrder = new LagerhotellAPI.Models.DbModels.Order { OrderId = order.Id, OrderDate = order.OrderDate, EndDate = order.EndDate, UserId = order.UserId, StorageUnitId = order.StorageUnitId, Status = order.Status, CustomInstructions = order.CustomInstructions };
+            await _orders.InsertOneAsync(dbOrder);
         }
         public async Task DeleteOrder(string orderId)
         {
@@ -22,21 +23,36 @@ namespace LagerhotellAPI.Services
         }
         public async Task ModifyOrder(string orderId, Order updatedOrder)
         {
-            await _orders.ReplaceOneAsync(order => order.Id == orderId, updatedOrder);
+            LagerhotellAPI.Models.DbModels.Order updatedDbOrder = new LagerhotellAPI.Models.DbModels.Order { OrderId = updatedOrder.Id, OrderDate = updatedOrder.OrderDate, EndDate = updatedOrder.EndDate, UserId = updatedOrder.UserId, StorageUnitId = updatedOrder.StorageUnitId, Status = updatedOrder.Status, CustomInstructions = updatedOrder.CustomInstructions };
+            await _orders.ReplaceOneAsync(order => order.Id == orderId, updatedDbOrder);
         }
-        public async Task<Order> GetOrder(string orderId)
+        public async Task<Order?> GetOrder(string orderId)
         {
-            return await _orders.Find(order => order.Id == orderId).FirstOrDefaultAsync();
+            var dbOrder = await _orders.Find(order => order.Id == orderId).FirstOrDefaultAsync();
+            if (dbOrder == null)
+            {
+                return null;
+            }
+            LagerhotellAPI.Models.DomainModels.Order domainOrder = new LagerhotellAPI.Models.DomainModels.Order { Id = dbOrder.OrderId, OrderDate = dbOrder.OrderDate, UserId = dbOrder.UserId, StorageUnitId = dbOrder.StorageUnitId, CustomInstructions = dbOrder.CustomInstructions };
+            return domainOrder;
         }
-        public async Task<List<Order>> GetAllOrders()
+        public async Task<List<LagerhotellAPI.Models.DomainModels.Order>> GetAllOrders()
         {
-            return await _orders.Find(_ => true).ToListAsync();
-        }
-
-        public async Task<List<Order>> GetAllOrdersByUser(User user)
-        {
-            var filter = Builders<Order>.Filter.Eq("UserId", user.Id);
-            return await _orders.Find(filter).ToListAsync();
+            List<LagerhotellAPI.Models.DbModels.Order> dbOrders = await _orders.Find(_ => true).ToListAsync();
+            List<LagerhotellAPI.Models.DomainModels.Order> domainOrders = dbOrders.ConvertAll(dbOrder =>
+            {
+                return new LagerhotellAPI.Models.DomainModels.Order
+                {
+                    Id = dbOrder.OrderId,
+                    OrderDate = dbOrder.OrderDate,
+                    EndDate = dbOrder.EndDate,
+                    UserId = dbOrder.UserId,
+                    StorageUnitId = dbOrder.StorageUnitId,
+                    Status = dbOrder.Status,
+                    CustomInstructions = dbOrder.CustomInstructions
+                };
+            });
+            return domainOrders;
         }
     }
 }
--- LagerhotellAPI/Services/StorageUnitService.cs
+++ LagerhotellAPI/Services/StorageUnitService.cs
@@ -5,20 +5,22 @@ namespace LagerhotellAPI.Services
 {
     public class StorageUnitService
     {
-        private readonly IMongoCollection<StorageUnit> _storageUnits;
+        private readonly IMongoCollection<LagerhotellAPI.Models.DbModels.StorageUnit> _storageUnits;
 
         public StorageUnitService(MongoDbSettings settings)
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase("Lagerhotell");
-            _storageUnits = database.GetCollection<StorageUnit>("StorageUnits");
+            _storageUnits = database.GetCollection<LagerhotellAPI.Models.DbModels.StorageUnit>("StorageUnits");
         }
 
         public async Task AddStorageUnit(StorageUnit storageUnit)
         {
             if (await GetStorageUnitById(storageUnit.Id) == null)
             {
-                await _storageUnits.InsertOneAsync(storageUnit);
+                LagerhotellAPI.Models.DbModels.StorageUnit dbStorageUnit = new LagerhotellAPI.Models.DbModels.StorageUnit { Length = storageUnit.Length, Width = storageUnit.Width, Height = storageUnit.Height, Temperated = storageUnit.Temperated, Area = storageUnit.Area, LockCode = storageUnit.LockCode, Name = storageUnit.Name, Occupied = storageUnit.Occupied, UserId = storageUnit.UserId, Coordinate = storageUnit.Coordinate, PricePerMonth = storageUnit.PricePerMonth };
+                dbStorageUnit.StorageUnitId = Guid.NewGuid().ToString();
+                await _storageUnits.InsertOneAsync(dbStorageUnit);
             }
             throw new InvalidOperationException("Already exists");
         }
@@ -35,19 +37,41 @@ namespace LagerhotellAPI.Services
         {
             if (await GetStorageUnitById(storageUnitId) != null)
             {
-                await _storageUnits.ReplaceOneAsync(unit => unit.Id == storageUnitId, updatedStorageUnit);
+                LagerhotellAPI.Models.DbModels.StorageUnit updatedDbStorageUnit = new LagerhotellAPI.Models.DbModels.StorageUnit { Id = updatedStorageUnit.Id, Length = updatedStorageUnit.Length, Width = updatedStorageUnit.Width, Height = updatedStorageUnit.Height, Temperated = updatedStorageUnit.Temperated, Area = updatedStorageUnit.Area, LockCode = updatedStorageUnit.LockCode, Name = updatedStorageUnit.Name, Occupied = updatedStorageUnit.Occupied, UserId = updatedStorageUnit.UserId, Coordinate = updatedStorageUnit.Coordinate, PricePerMonth = updatedStorageUnit.PricePerMonth };
+                await _storageUnits.ReplaceOneAsync(unit => unit.Id == storageUnitId, updatedDbStorageUnit);
             }
             throw new KeyNotFoundException();
         }
 
         public async Task<StorageUnit> GetStorageUnitById(string storageUnitId)
         {
-            return await _storageUnits.Find(unit => unit.Id == storageUnitId).FirstOrDefaultAsync();
+            var dbStorageUnit = await _storageUnits.Find(unit => unit.Id == storageUnitId).FirstOrDefaultAsync();
+            var domainStorageUnit = new LagerhotellAPI.Models.DomainModels.StorageUnit { Id = dbStorageUnit.Id, Length = dbStorageUnit.Length, Width = dbStorageUnit.Width, Height = dbStorageUnit.Height, Temperated = dbStorageUnit.Temperated, Area = dbStorageUnit.Area, LockCode = dbStorageUnit.LockCode, Name = dbStorageUnit.Name, Occupied = dbStorageUnit.Occupied, UserId = dbStorageUnit.UserId, Coordinate = dbStorageUnit.Coordinate, PricePerMonth = dbStorageUnit.PricePerMonth };
+            return domainStorageUnit;
         }
 
-        public async Task<List<StorageUnit>> GetAllStorageUnits()
+        public async Task<List<LagerhotellAPI.Models.DomainModels.StorageUnit>> GetAllStorageUnits()
         {
-            return await _storageUnits.Find(_ => true).ToListAsync();
+            List<LagerhotellAPI.Models.DbModels.StorageUnit> dbStorageUnits = await _storageUnits.Find(_ => true).ToListAsync();
+            List<LagerhotellAPI.Models.DomainModels.StorageUnit> domainStorageUnits = dbStorageUnits.ConvertAll(dbStorageUnit =>
+            {
+                return new LagerhotellAPI.Models.DomainModels.StorageUnit
+                {
+                    Id = dbStorageUnit.StorageUnitId,
+                    Length = dbStorageUnit.Length,
+                    Width = dbStorageUnit.Width,
+                    Height = dbStorageUnit.Height,
+                    Temperated = dbStorageUnit.Temperated,
+                    Area = dbStorageUnit.Area,
+                    LockCode = dbStorageUnit.LockCode,
+                    Name = dbStorageUnit.Name,
+                    Occupied = dbStorageUnit.Occupied,
+                    UserId = dbStorageUnit.UserId,
+                    Coordinate = dbStorageUnit.Coordinate,
+                    PricePerMonth = dbStorageUnit.PricePerMonth,
+                };
+            });
+            return domainStorageUnits;
         }
     }
 }
