35eb0bae0914b88005b974fc5720ed50e6381896
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-06 13:16:44 +0100

    Implement on domain models

---
+++ Lagerhotell.Shared/DomainModels/User.cs
@@ -0,0 +1,29 @@
+﻿namespace LagerhotellAPI.Models.DomainModels;
+public class User
+{
+    public User(string userId, string firstName, string lastName, string phoneNumber, string birthDate, Address address, string password)
+    {
+        Id = userId;
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        BirthDate = birthDate;
+        Address = address;
+        Password = password;
+    }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public User()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+
+    }
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string BirthDate { get; set; }
+
+    public Address Address { get; set; }
+    public string Password { get; set; }
+}
