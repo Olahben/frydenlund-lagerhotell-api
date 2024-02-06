df50575b63433b955033b66806a34143782d3cc0
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-06 13:16:14 +0100

    Add constructor

--- Lagerhotell.Shared/DomainModels/Order.cs
+++ Lagerhotell.Shared/DomainModels/Order.cs
@@ -1,21 +1,22 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿namespace LagerhotellAPI.Models.DomainModels;
 
-namespace LagerhotellAPI.Models
+public class Order
 {
-    public class Order
+    public Order(string orderId, OrderPeriod orderPeriod, string userId, string storageUnitId, OrderStatus status, string? customInstructions = null)
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public required string Id { get; set; }
-        public required DateTime OrderDate { get; set; }
-
-        public DateTime? EndDate { get; set; }
-        public required string UserId { get; set; }
-        public required string StorageUnitId { get; set; }
-        public OrderStatus Status { get; set; }
-
-        public string? CustomInstructions { get; set; }
-
+        Id = orderId;
+        OrderPeriod = orderPeriod;
+        UserId = userId;
+        StorageUnitId = storageUnitId;
+        Status = status;
+        CustomInstructions = customInstructions;
     }
+    public string Id { get; set; }
+    public OrderPeriod OrderPeriod { get; set; }
+    public string UserId { get; set; }
+    public string StorageUnitId { get; set; }
+    public OrderStatus Status { get; set; }
+
+    public string? CustomInstructions { get; set; }
+
 }
--- Lagerhotell.Shared/DomainModels/StorageUnit.cs
+++ Lagerhotell.Shared/DomainModels/StorageUnit.cs
@@ -1,37 +1,32 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿namespace LagerhotellAPI.Models.DomainModels;
 
-namespace LagerhotellAPI.Models
+public class StorageUnit
 {
-    public class StorageUnit
+    public StorageUnit(string storageUnitId, Dimensions dimensions, bool temperated, string lockCode, string name, bool occupied, string userId, Coordinate coordinate, Money pricePerMonth)
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public required string Id { get; set; }
-
-        public required double Length { get; set; }
-
-        public required double Width { get; set; }
-
-        public required double Height { get; set; }
-
-        public required bool Temperated { get; set; }
-
-        public required string Area { get; set; }
-        public string? LockCode { get; set; }
-
-        public required string Name { get; set; }
-
-        public double AreaSize => Length * Width;
-
-        public double Volume => Length * Width * Height;
-
-        public bool Occupied { get; set; } = false;
-
-        public string? UserId { get; set; }
-
-        public Coordinate? Coordinate { get; set; }
-
-        public required double PricePerMonth { get; set; }
+        Id = storageUnitId;
+        Dimensions = dimensions;
+        Temperated = temperated;
+        LockCode = lockCode;
+        Name = name;
+        Occupied = occupied;
+        UserId = userId;
+        Coordinate = coordinate;
+        PricePerMonth = pricePerMonth;
     }
+    public string? Id { get; set; }
+    public Dimensions Dimensions { get; set; }
+
+    public bool Temperated { get; set; }
+
+    public string? LockCode { get; set; }
+
+    public string Name { get; set; }
+    public bool Occupied { get; set; } = false;
+
+    public string? UserId { get; set; }
+
+    public Coordinate? Coordinate { get; set; }
+
+    public Money PricePerMonth { get; set; }
 }
