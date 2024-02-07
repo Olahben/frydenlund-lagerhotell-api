a3e9201df6ca9a12d394d151dfe7c408a1021445
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-07 10:52:04 +0100

    Implement

---
+++ Lagerhotell.Shared/DbModels/WarehouseHotel.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LagerhotellAPI.Models.DbModels;
+
+public class WarehouseHotel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public string? WarehouseHotelId { get; set; }
+    public Coordinate Coordinate { get; set; }
+    public Address Address { get; set; }
+    public string Name { get; set; }
+    public OpeningHours OpeningHours { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? DetailedDescription { get; set; }
+    public bool ContainsTemperatedStorageUnits { get; set; }
+    public StorageUnitSizes StorageUnitsSizes { get; set; }
+
+    public WarehouseHotel(Coordinate coordinate, Address address, string name, OpeningHours openingHours, string phoneNumber, bool containsTemperatedStorageUnits, StorageUnitSizes storageUnitSizes)
+    {
+        Coordinate = coordinate;
+        Address = address;
+        Name = name;
+        OpeningHours = openingHours;
+        PhoneNumber = phoneNumber;
+        ContainsTemperatedStorageUnits = containsTemperatedStorageUnits;
+        StorageUnitsSizes = storageUnitSizes;
+    }
+
+}
---
+++ Lagerhotell.Shared/DomainModels/WarehouseHotel.cs
@@ -0,0 +1,25 @@
+﻿namespace LagerhotellAPI.Models.DomainModels;
+
+public class WarehouseHotel
+{
+    public string? WarehouseHotelId { get; set; }
+    public Coordinate Coordinate { get; set; }
+    public Address Address { get; set; }
+    public string Name { get; set; }
+    public OpeningHours OpeningHours { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? DetailedDescription { get; set; }
+    public bool ContainsTemperatedStorageUnits { get; set; }
+    public StorageUnitSizes StorageUnitsSizes { get; set; }
+
+    public WarehouseHotel(Coordinate coordinate, Address address, string name, OpeningHours openingHours, string phoneNumber, bool containsTemperatedStorageUnits, StorageUnitSizes storageUnitSizes)
+    {
+        Coordinate = coordinate;
+        Address = address;
+        Name = name;
+        OpeningHours = openingHours;
+        PhoneNumber = phoneNumber;
+        ContainsTemperatedStorageUnits = containsTemperatedStorageUnits;
+        StorageUnitsSizes = storageUnitSizes;
+    }
+}
