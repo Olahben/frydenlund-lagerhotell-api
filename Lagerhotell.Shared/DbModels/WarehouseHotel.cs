3a314095777590a527e740cf911bdf38651b65d8
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-07 12:26:43 +0100

    Add ctor

--- Lagerhotell.Shared/DbModels/WarehouseHotel.cs
+++ Lagerhotell.Shared/DbModels/WarehouseHotel.cs
@@ -14,17 +14,30 @@ public class WarehouseHotel
     public string Name { get; set; }
     public OpeningHours OpeningHours { get; set; }
     public string PhoneNumber { get; set; }
-    public string? DetailedDescription { get; set; }
+    public string DetailedDescription { get; set; }
     public bool ContainsTemperatedStorageUnits { get; set; }
     public StorageUnitSizes StorageUnitsSizes { get; set; }
 
-    public WarehouseHotel(Coordinate coordinate, Address address, string name, OpeningHours openingHours, string phoneNumber, bool containsTemperatedStorageUnits, StorageUnitSizes storageUnitSizes)
+    public WarehouseHotel(Coordinate coordinate, Address address, string name, OpeningHours openingHours, string phoneNumber, string detailedDescription, bool containsTemperatedStorageUnits, StorageUnitSizes storageUnitSizes)
     {
         Coordinate = coordinate;
         Address = address;
         Name = name;
         OpeningHours = openingHours;
         PhoneNumber = phoneNumber;
+        DetailedDescription = detailedDescription;
+        ContainsTemperatedStorageUnits = containsTemperatedStorageUnits;
+        StorageUnitsSizes = storageUnitSizes;
+    }
+    public WarehouseHotel(string warehouseHotelId, Coordinate coordinate, Address address, string name, OpeningHours openingHours, string phoneNumber, string detailedDescription, bool containsTemperatedStorageUnits, StorageUnitSizes storageUnitSizes)
+    {
+        WarehouseHotelId = warehouseHotelId;
+        Coordinate = coordinate;
+        Address = address;
+        Name = name;
+        OpeningHours = openingHours;
+        PhoneNumber = phoneNumber;
+        DetailedDescription = detailedDescription;
         ContainsTemperatedStorageUnits = containsTemperatedStorageUnits;
         StorageUnitsSizes = storageUnitSizes;
     }
