a620302ef5834fdc7e667f68effe4553bd9e2c1d
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-07 10:30:04 +0100

    Implement

---
+++ Lagerhotell.Shared/ValueTypes/OpeningHours.cs
@@ -0,0 +1,16 @@
+﻿namespace LagerhotellAPI.Models.ValueTypes;
+
+public class OpeningHours
+{
+    public double Opens { get; set; }
+    public double Closes { get; set; }
+    public OpeningHours(double opens, double closes)
+    {
+        if (opens < 0 || closes < 0 || opens > Closes)
+        {
+            throw new ArgumentException("Feil");
+        }
+        Opens = opens;
+        Closes = closes;
+    }
+}
---
+++ Lagerhotell.Shared/ValueTypes/StorageUnitSizes.cs
@@ -0,0 +1,17 @@
+﻿namespace LagerhotellAPI.Models.ValueTypes;
+
+public class StorageUnitSizes
+{
+    public double MinSize { get; set; }
+    public double MaxSize { get; set; } = 0;
+
+    public StorageUnitSizes(double minSize, double maxSize)
+    {
+        if (MinSize < 0 || MaxSize < 0 || MinSize > MaxSize)
+        {
+            throw new ArgumentException("Feil");
+        }
+        MinSize = minSize;
+        MaxSize = maxSize;
+    }
+}
