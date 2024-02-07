09dc2ec6ab334bb0f3f85b0fa6ed795210f89dfe
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-07 12:35:16 +0100

    Throw exceptions

--- LagerhotellAPI/Services/WarehouseHotelService.cs
+++ LagerhotellAPI/Services/WarehouseHotelService.cs
@@ -32,15 +32,23 @@ public class WarehouseHotelService
     public async Task<WarehouseHotel> GetWarehouseHotelById(string id)
     {
         LagerhotellAPI.Models.DbModels.WarehouseHotel dbWarehouseHotel = await _warehouseHotels.Find(hotel => hotel.Id == id).FirstOrDefaultAsync();
+        if (dbWarehouseHotel == null || dbWarehouseHotel.WarehouseHotelId == null)
+        {
+            throw new KeyNotFoundException();
+        }
         WarehouseHotel domainWarehouseHotel = new(dbWarehouseHotel.WarehouseHotelId, dbWarehouseHotel.Coordinate, dbWarehouseHotel.Address, dbWarehouseHotel.Name, dbWarehouseHotel.OpeningHours, dbWarehouseHotel.PhoneNumber, dbWarehouseHotel.DetailedDescription, dbWarehouseHotel.ContainsTemperatedStorageUnits, dbWarehouseHotel.StorageUnitsSizes);
         return domainWarehouseHotel;
     }
 
-    public async Task<List<WarehouseHotel>> GetAllWarehouseHotels(int skip, int take)
+    public async Task<List<WarehouseHotel>> GetAllWarehouseHotels(int? skip = 0, int? take = 0)
     {
         List<Models.DbModels.WarehouseHotel> dbWarehouseHotels = await _warehouseHotels.Find(hotel => true).Skip(skip).Limit(take).ToListAsync();
         List<WarehouseHotel> domainWarehouseHotels = dbWarehouseHotels.ConvertAll(hotel =>
         {
+            if (hotel == null || hotel.WarehouseHotelId == null)
+            {
+                throw new KeyNotFoundException();
+            }
             return new WarehouseHotel(hotel.WarehouseHotelId, hotel.Coordinate, hotel.Address, hotel.Name, hotel.OpeningHours, hotel.PhoneNumber, hotel.DetailedDescription, hotel.ContainsTemperatedStorageUnits, hotel.StorageUnitsSizes);
         });
         return domainWarehouseHotels;
