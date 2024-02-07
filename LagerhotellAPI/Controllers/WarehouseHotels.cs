df0a191a39cb1fc7ecfd2a3c23db238d9f145a22
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-07 12:50:21 +0100

    Implement

---
+++ LagerhotellAPI/Controllers/WarehouseHotels.cs
@@ -0,0 +1,67 @@
+﻿using LagerhotellAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LagerhotellAPI.Controllers;
+
+[Route("api/warehouse-hotels")]
+[ApiController]
+public class WarehouseHotelsController : ControllerBase
+{
+    private readonly WarehouseHotelService _warehouseHotelService;
+
+    public WarehouseHotelsController(WarehouseHotelService warehouseHotelService)
+    {
+        _warehouseHotelService = warehouseHotelService;
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public async Task<IActionResult> AddWarehouseHotel([FromBody] WarehouseHotel warehouseHotel)
+    {
+        await _warehouseHotelService.AddWarehouseHotel(warehouseHotel);
+        return Ok("Warehouse hotel added successfully.");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteWarehouseHotel(string id)
+    {
+        await _warehouseHotelService.DeleteWarehouseHotel(id);
+        return Ok("Warehouse hotel deleted successfully.");
+    }
+
+    [HttpPut]
+    [Route("modify")]
+    public async Task<IActionResult> ModifyWarehouseHotel([FromBody] WarehouseHotel warehouseHotel)
+    {
+        await _warehouseHotelService.ModifyWarehouseHotel(warehouseHotel);
+        return Ok("Warehouse hotel modified successfully.");
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetWarehouseHotelById(string id)
+    {
+        try
+        {
+            var warehouseHotel = await _warehouseHotelService.GetWarehouseHotelById(id);
+            return Ok(warehouseHotel);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllWarehouseHotels([FromQuery] int? skip, int? take)
+    {
+        try
+        {
+            var warehouseHotels = await _warehouseHotelService.GetAllWarehouseHotels(skip, take);
+            return Ok(warehouseHotels);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+}
