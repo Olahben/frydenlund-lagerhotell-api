a6be6351930ce582775db44ffa3b4c636438fe79
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-09 15:05:19 +0100

    Implement

---
+++ Lagerhotell.Shared/DbModels/Location.cs
@@ -0,0 +1,18 @@
+﻿global using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LagerhotellAPI.Models.DbModels;
+
+public class Location
+{
+    public Location(string name)
+    {
+        Name = name;
+    }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+}
---
+++ Lagerhotell.Shared/DomainModels/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace LagerhotellAPI.Models.DomainModels;
+
+public class Location
+{
+    public Location(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
