1b77997f2af201a8889b0bb259a97c636dde0490
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-02-16 12:23:43 +0100

    Implement

---
+++ LagerhotellAPI/Controllers/Locations.cs
@@ -0,0 +1,88 @@
+﻿using LagerhotellAPI.Models;
+using LagerhotellAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers;
+
+[ApiController]
+[Route("locations")]
+public class LocationsController : ControllerBase
+{
+    private readonly LocationService _locationService;
+
+    public LocationsController(LocationService locationService)
+    {
+        _locationService = locationService;
+    }
+
+    [HttpPost]
+    [Route("add")]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> AddLocation([FromBody] AddLocationRequest request)
+    {
+        try
+        {
+            await _locationService.Add(request.Location);
+            return Ok("Location was added");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Error in Locations controller", ex);
+            return Conflict("Error in adding location, location probably already exists");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error in locations controller", ex);
+            return StatusCode(500);
+        }
+    }
+
+    [HttpDelete("{name}")]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> DeleteLocation([FromRoute] string name)
+    {
+        try
+        {
+            await _locationService.Delete(name);
+            return Ok("Location was deleted");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Error in locations controller DeleteLocation method, location does probably not exist. Error: {ex}");
+            return Conflict();
+        }
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "Administrator")]
+    [Route("modify")]
+    public async Task<IActionResult> ModifyLocation([FromBody] ModifyLocationRequest request)
+    {
+        try
+        {
+            await _locationService.Modify(request.Location, request.OldLocationName);
+            return Ok($"Location with the name {request.OldLocationName} was successfully changed to {request.Location.Name}");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Error in locations controller ModifyLocation, location does probably not exist. Error: {ex}");
+            return Conflict();
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllLocations([FromQuery] bool includeNonActive, int? skip, int? take)
+    {
+        try
+        {
+            List<Location> locations = await _locationService.GetAll(includeNonActive, skip, take);
+            return Ok(new GetAllLocationsResponse(locations));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in locations controller GetAllLocations, Error: {ex}");
+            return StatusCode(500);
+        }
+    }
+}
