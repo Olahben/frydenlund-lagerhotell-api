ac63ab133f9fc5d4733fc5caa11c8cee63665e3b
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-04-12 14:04:48 +0200

    Create, build ctor

---
+++ LagerhotellAPI/Services/AssetService.cs
@@ -0,0 +1,15 @@
+﻿using LagerhotellAPI.Models;
+using MongoDB.Driver;
+
+namespace LagerhotellAPI.Services;
+
+public class AssetService
+{
+    private readonly IMongoCollection<Models.DbModels.ImageAsset> _assets;
+    public AssetService(MongoDbSettings settings)
+    {
+        var client = new MongoClient(settings.ConnectionString);
+        var database = client.GetDatabase("Lagerhotell");
+        _assets = database.GetCollection<Models.DbModels.ImageAsset>("Assets");
+    }
+}
