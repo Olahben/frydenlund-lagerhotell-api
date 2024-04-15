af6a70698d06c847a7d7b5142eff651bffd2f12f
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-04-15 19:06:32 +0200

    Create and test

---
+++ LagerhotellAPI/Controllers/Assets.cs
@@ -0,0 +1,98 @@
+﻿using LagerhotellAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers;
+
+[ApiController]
+[Route("assets")]
+public class AssetsController : ControllerBase
+{
+    private readonly AssetService _assetService;
+
+    public AssetsController(AssetService assetService)
+    {
+        _assetService = assetService;
+    }
+
+    // Add validation of requests here
+
+    [HttpPost]
+    [Route("add")]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> AddAsset([FromBody] AddImageAssetRequest request)
+    {
+        try
+        {
+            string assetId = await _assetService.AddAsset(request.Asset);
+            return Ok(new AddAssetResponse() { AssetId = assetId });
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"{ex}");
+            return Conflict("Asset already exists");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex}");
+            return StatusCode(500);
+        }
+    }
+
+    [HttpDelete("{assetId}")]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> DeleteAsset([FromRoute] string assetId)
+    {
+        try
+        {
+            await _assetService.DeleteAsset(assetId);
+            return Ok("Asset was deleted");
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpPut]
+    [Route("modify")]
+    [Authorize(Roles = "Administrator")]
+    public async Task<IActionResult> ModifyAsset([FromBody] ModifyAssetRequest request)
+    {
+        try
+        {
+            await _assetService.ModifyAsset(request.AssetId, request.Asset);
+            return Ok("Asset was modified");
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet("{assetId}")]
+    public async Task<IActionResult> GetAsset([FromRoute] string assetId)
+    {
+        ImageAsset? asset = await _assetService.GetAsset(assetId);
+        if (asset == null)
+        {
+            return NotFound();
+        }
+        return Ok(asset);
+    }
+
+    [HttpGet("{skip}/{take}/{warehouseHotelId}")]
+    public async Task<IActionResult> GetAllAssets(int? skip, int? take, string? warehouseHotelId)
+    {
+        try
+        {
+            List<ImageAsset> assets = await _assetService.GetAssets(skip, take, warehouseHotelId);
+            return Ok(new GetAllAssetsResponse() { Assets = assets });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex}");
+            return StatusCode(500);
+        }
+    }
+}
