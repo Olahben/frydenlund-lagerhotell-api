8b36d2b4edfa653ce13fae9584330b182a9b649f
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-10 18:27:11 +0200

    Create

---
+++ Lagerhotell.Shared/ValueTypes/StorageUnitSizesGroup.cs
@@ -0,0 +1,19 @@
+﻿namespace LagerhotellAPI.Models.ValueTypes;
+
+public enum StorageUnitSizesGroup
+{
+    // The sizes are based on the area of the storage unit in square meters
+    // 0-3 m^2
+    xs,
+    // 3-6 m^2
+    sm,
+    // 6-12 m^2
+    md,
+    // 12-18 m^2
+    lg,
+    // 18-24 m^2
+    xl,
+    // 24-30 m^2
+    xxl
+    // 30+ m^2
+}
