3e4158a3fa987356ddfac6c8d4d814e5667115ea
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-10 18:13:59 +0200

    Create

---
+++ Lagerhotell.Shared/DbModels/InfoText.cs
@@ -0,0 +1,8 @@
+﻿namespace LagerhotellAPI.Models.DbModels;
+using LagerhotellAPI.Models.FrontendModels.Custom;
+
+public class InfoText
+{
+    public string Text { get; set; }
+    public InfoTextType Type { get; set; }
+}
