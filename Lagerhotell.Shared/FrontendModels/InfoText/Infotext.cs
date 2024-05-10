c79dc5181fdc87a97802e9c5df47f6e43cbc2c79
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-10 18:31:21 +0200

    Add field "StorageUnitSizeGroup"

--- Lagerhotell.Shared/FrontendModels/InfoText/Infotext.cs
+++ Lagerhotell.Shared/FrontendModels/InfoText/Infotext.cs
@@ -1,7 +1,8 @@
-﻿namespace LagerhotellAPI.Models.FrontendModels.Custom;
+﻿namespace LagerhotellAPI.Models.DomainModels;
 
 public class InfoText
 {
     public string Text { get; set; }
     public InfoTextType Type { get; set; } = 0;
+    public StorageUnitSizesGroup? StorageUnitSizeGroup { get; set; }
 }
