7df124f35efc0570d86f1a0370449e5fd308ce53
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-10 18:28:59 +0200

    Add "Id", and "StorageUnitSizeGroup" fields

--- Lagerhotell.Shared/DbModels/InfoText.cs
+++ Lagerhotell.Shared/DbModels/InfoText.cs
@@ -1,8 +1,12 @@
 ﻿namespace LagerhotellAPI.Models.DbModels;
-using LagerhotellAPI.Models.FrontendModels.Custom;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class InfoText
 {
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string Id { get; set; }
     public string Text { get; set; }
     public InfoTextType Type { get; set; }
+    public StorageUnitSizesGroup? StorageUnitSizeGroup { get; set; }
 }
