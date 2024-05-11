871f945e48b8bd40ee92afb6d52b71866028c42e
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-11 15:50:56 +0200

    Add a custom ID field

--- Lagerhotell.Shared/DbModels/InfoText.cs
+++ Lagerhotell.Shared/DbModels/InfoText.cs
@@ -6,6 +6,7 @@ public class InfoText
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
     public string Id { get; set; }
+    public string InfoTextId { get; set; }
     public string Text { get; set; }
     public InfoTextType Type { get; set; }
     public StorageUnitSizesGroup? StorageUnitSizeGroup { get; set; }
--- Lagerhotell.Shared/FrontendModels/InfoText/Infotext.cs
+++ Lagerhotell.Shared/FrontendModels/InfoText/Infotext.cs
@@ -2,6 +2,7 @@
 
 public class InfoText
 {
+    public string InfoTextId { get; set; }
     public string Text { get; set; }
     public InfoTextType Type { get; set; } = 0;
     public StorageUnitSizesGroup? StorageUnitSizeGroup { get; set; }
