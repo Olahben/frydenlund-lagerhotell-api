ff7adf64498e76d4008c611ff529722f0d132c3a
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-11 15:58:10 +0200

    Add method "ModifyInfoText", and add summaries

--- LagerhotellAPI/Services/InfoTextService.cs
+++ LagerhotellAPI/Services/InfoTextService.cs
@@ -15,17 +15,32 @@ public class InfoTextService
         _infoTexts = database.GetCollection<Models.DbModels.InfoText>("InfoTexts");
     }
 
+    /// <summary>
+    /// Gets all InfoTexts
+    /// </summary>
+    /// <returns></returns>
     public async Task<List<Models.DbModels.InfoText>> GetInfoTexts()
     {
         return await _infoTexts.Find(infoText => true).ToListAsync();
     }
 
+    /// <summary>
+    /// Gets the InfoText with the given storage unit size group
+    /// </summary>
+    /// <param name="sizeGroup"></param>
+    /// <returns></returns>
     public async Task<Models.DbModels.InfoText?> GetInfoTextStorageUnit(StorageUnitSizesGroup? sizeGroup)
     {
         if (sizeGroup == null) { return null; }
         return await _infoTexts.Find(infoText => infoText.StorageUnitSizeGroup == sizeGroup).FirstOrDefaultAsync();
     }
 
+    /// <summary>
+    /// Creates a new InfoText with the given InfoText and assigns a new InfoTextId
+    /// </summary>
+    /// <param name="infoText"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public async Task<string> CreateInfoText(Models.DomainModels.InfoText infoText)
     {
         if (infoText.StorageUnitSizeGroup != null)
@@ -35,10 +50,29 @@ public class InfoTextService
                 throw new InvalidOperationException("InfoText for this storage unit size group already exists");
             }
         }
-        Models.DbModels.InfoText dbInfoText = new() { Text = infoText.Text, Type = infoText.Type, StorageUnitSizeGroup = infoText.StorageUnitSizeGroup };
+        Models.DbModels.InfoText dbInfoText = new() { Text = infoText.Text, Type = infoText.Type, StorageUnitSizeGroup = infoText.StorageUnitSizeGroup, InfoTextId = Guid.NewGuid().ToString() };
         await _infoTexts.InsertOneAsync(dbInfoText);
         return dbInfoText.Id;
     }
+    /// <summary>
+    /// Modifies a InfoText with given InfoTextId with the given InfoText
+    /// </summary>
+    /// <param name="infoTextId"></param>
+    /// <param name="infoText"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public async Task ModifyInfoText(string infoTextId, Models.DomainModels.InfoText infoText)
+    {
+        Models.DbModels.InfoText dbInfoText = await _infoTexts.Find(infoText => infoText.InfoTextId == infoTextId).FirstOrDefaultAsync();
+        if (dbInfoText == null)
+        {
+            throw new InvalidOperationException("InfoText with given InfoTextId does not exist");
+        }
+        dbInfoText.Text = infoText.Text;
+        dbInfoText.Type = infoText.Type;
+        dbInfoText.StorageUnitSizeGroup = infoText.StorageUnitSizeGroup;
 
+        await _infoTexts.ReplaceOneAsync(infoText => infoText.InfoTextId == infoTextId, dbInfoText);
+    }
 
 }
