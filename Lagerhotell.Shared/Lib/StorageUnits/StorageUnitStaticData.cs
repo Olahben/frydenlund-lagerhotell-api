94b14fb4c28027d7241547b98d17293e3fe095c5
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-24 09:31:42 +0200

    Make StorageUnitSizeDescriptions immutable

--- Lagerhotell.Shared/Lib/StorageUnits/StorageUnitStaticData.cs
+++ Lagerhotell.Shared/Lib/StorageUnits/StorageUnitStaticData.cs
@@ -1,28 +1,28 @@
-﻿namespace Lagerhotell.Shared.Lib
-{
-    public class StorageUnitStaticData
-    {
-        public enum StorageUnitInfoTexts
-        {
-            XSmall, //1-3M2
-            Small, //4-8M2
-            Medium, //9-12M2
-            Large, //13-18M2
-            XLarge //19-24M2
-            /*"I dette lageret vil du kunne få plass til møbler og annet diverse fra en liten/normal enromsleilighet eller fra ett vanlig soverom.", //1-3M2
-            "I dette lageret vil du kunne få plass til innholdet til en liten normalt møblert leilighet inkludert noen andre eiendeler.", //4-8M2
-            "I dette lageret vil du kunne få plass til innholdet til en medium stor leilighet som er normalt møblert.", //9-12M2
-            "I dette lageret vil du kunne få plass til innholdet til en større leilighet som er normalt møblert.", //13-18M2
-            "I dette lageret vil du kunne få plass til innholdet til en større enemannsbolig som er normalt møblert.", //19-24M2 */
-        }
+﻿namespace Lagerhotell.Shared.Lib;
+using System.Collections.Immutable;
 
-        public static readonly Dictionary<StorageUnitInfoTexts, string> StorageUnitSizeDescriptions = new Dictionary<StorageUnitInfoTexts, string>
+public class StorageUnitStaticData
+{
+    public enum StorageUnitInfoTexts
     {
-        { StorageUnitInfoTexts.XSmall, "I dette lageret vil du kunne få plass til møbler og annet diverse fra en liten/normal enromsleilighet eller fra ett vanlig soverom." },
-        { StorageUnitInfoTexts.Small, "I dette lageret vil du kunne få plass til innholdet til en liten normalt møblert leilighet inkludert noen andre eiendeler." },
-        { StorageUnitInfoTexts.Medium, "I dette lageret vil du kunne få plass til innholdet til en medium stor leilighet som er normalt møblert." },
-        { StorageUnitInfoTexts.Large, "I dette lageret vil du kunne få plass til innholdet til en større leilighet som er normalt møblert." },
-        { StorageUnitInfoTexts.XLarge, "I dette lageret vil du kunne få plass til innholdet til en større enmannsbolig som er normalt møblert i tillegg til fritidsutstyr." }
-    };
+        XSmall, //1-3M2
+        Small, //4-8M2
+        Medium, //9-12M2
+        Large, //13-18M2
+        XLarge //19-24M2
+        /*"I dette lageret vil du kunne få plass til møbler og annet diverse fra en liten/normal enromsleilighet eller fra ett vanlig soverom.", //1-3M2
+        "I dette lageret vil du kunne få plass til innholdet til en liten normalt møblert leilighet inkludert noen andre eiendeler.", //4-8M2
+        "I dette lageret vil du kunne få plass til innholdet til en medium stor leilighet som er normalt møblert.", //9-12M2
+        "I dette lageret vil du kunne få plass til innholdet til en større leilighet som er normalt møblert.", //13-18M2
+        "I dette lageret vil du kunne få plass til innholdet til en større enemannsbolig som er normalt møblert.", //19-24M2 */
     }
+
+    public static readonly ImmutableDictionary<StorageUnitInfoTexts, string> StorageUnitSizeDescriptions = new Dictionary<StorageUnitInfoTexts, string>
+{
+    { StorageUnitInfoTexts.XSmall, "I dette lageret vil du kunne få plass til møbler og annet diverse fra en liten/normal enromsleilighet eller fra ett vanlig soverom." },
+    { StorageUnitInfoTexts.Small, "I dette lageret vil du kunne få plass til innholdet til en liten normalt møblert leilighet inkludert noen andre eiendeler." },
+    { StorageUnitInfoTexts.Medium, "I dette lageret vil du kunne få plass til innholdet til en medium stor leilighet som er normalt møblert." },
+    { StorageUnitInfoTexts.Large, "I dette lageret vil du kunne få plass til innholdet til en større leilighet som er normalt møblert." },
+    { StorageUnitInfoTexts.XLarge, "I dette lageret vil du kunne få plass til innholdet til en større enmannsbolig som er normalt møblert i tillegg til fritidsutstyr." }
+}.ToImmutableDictionary();
 }
