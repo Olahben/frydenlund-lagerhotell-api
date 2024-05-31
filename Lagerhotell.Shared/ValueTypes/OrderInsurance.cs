ed86e53136bea71629c4ec8426f3b3485605d526
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-05-31 11:22:36 +0200

    Create

---
+++ Lagerhotell.Shared/ValueTypes/OrderInsurance.cs
@@ -0,0 +1,10 @@
+﻿namespace LagerhotellAPI.Models.ValueTypes;
+
+public enum OrderInsurance
+{
+    None, // No insurance
+    Low, // Up to 50 000KR
+    Medium, // Up to 100 000KR
+    High, // Up to 200 000KR
+    Full, // Up to 500 000KR
+}
