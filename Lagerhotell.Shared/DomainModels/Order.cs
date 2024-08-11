02147f53c4f6fe52ea1aef47dada2b267506f600
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-11 22:37:51 +0200

    fix order status and order insurance properties

--- Lagerhotell.Shared/DomainModels/Order.cs
+++ Lagerhotell.Shared/DomainModels/Order.cs
@@ -1,3 +1,3 @@
 ﻿namespace LagerhotellAPI.Models.DomainModels;
 
-public record Order(string OrderId, string UserId, string StorageUnitId, OrderPeriod OrderPeriod, OrderStatus Status, OrderInsurance Insurance, string? CustomInstructions = null);
+public record Order(string OrderId, string UserId, string StorageUnitId, OrderPeriod OrderPeriod, OrderStatus Status = OrderStatus.NotCreated, OrderInsurance Insurance = OrderInsurance.FiftyThousand, string? CustomInstructions = null);
