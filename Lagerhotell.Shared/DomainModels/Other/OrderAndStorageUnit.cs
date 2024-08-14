fe95b57e703ab380e7090be9d07b86f5154d62d4
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-14 10:45:30 +0200

    Create

---
+++ Lagerhotell.Shared/DomainModels/Other/OrderAndStorageUnit.cs
@@ -0,0 +1,4 @@
+﻿namespace LagerhotellAPI.Models.DomainModels;
+
+// Used to display orders and storage units together when both of their data is needed
+public record OrderAndStorageUnit(Order Order, StorageUnit StorageUnit);
