03793550918fcd49ed989f10768f1de1d8cbe0b2
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-14 10:57:53 +0200

    Rename and only use necessary values

--- Lagerhotell.Shared/DomainModels/Other/OrderAndStorageUnit.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace LagerhotellAPI.Models.DomainModels;
-
-// Used to display orders and storage units together when both of their data is needed
-public record OrderAndStorageUnit(List<Order> Orders, List<StorageUnit> StorageUnits);
