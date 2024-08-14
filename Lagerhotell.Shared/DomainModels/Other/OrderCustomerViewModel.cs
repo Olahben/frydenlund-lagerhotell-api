8ff83a387f575d0c9d16fac402356994f227c259
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-14 20:08:18 +0200

    Fix fields

--- Lagerhotell.Shared/DomainModels/Other/OrderCustomerViewModel.cs
+++ Lagerhotell.Shared/DomainModels/Other/OrderCustomerViewModel.cs
@@ -1,4 +1,4 @@
 ﻿namespace LagerhotellAPI.Models.DomainModels;
 
 // Used to display orders and storage units in the "orders" section of the customer page
-public record OrderCustomerViewModel(OrderStatus Status, Dimensions Dimensions, string WarehouseHotelName, OrderPeriod OrderPeriod, Money Price);
+public record OrderCustomerViewModel(Order Order, StorageUnit StorageUnit, string WarehouseHotelName);
