b2a8cc4540fe0bcaa47ff20876133843f5125ff1
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-14 09:53:38 +0200

    Remove record, add parameterless constructor

--- Lagerhotell.Shared/DomainModels/Order.cs
+++ Lagerhotell.Shared/DomainModels/Order.cs
@@ -1,3 +1,28 @@
-﻿namespace LagerhotellAPI.Models.DomainModels;
+﻿namespace LagerhotellAPI.Models.DomainModels
+{
+    public record Order
+    {
+        public string OrderId { get; init; }
+        public string UserId { get; init; }
+        public string StorageUnitId { get; init; }
+        public OrderPeriod OrderPeriod { get; init; }
+        public OrderStatus? Status { get; init; } = OrderStatus.NotCreated;
+        public OrderInsurance? Insurance { get; init; } = OrderInsurance.FiftyThousand;
+        public string? CustomInstructions { get; init; }
 
-public record Order(string OrderId, string UserId, string StorageUnitId, OrderPeriod OrderPeriod, OrderStatus Status = OrderStatus.NotCreated, OrderInsurance Insurance = OrderInsurance.FiftyThousand, string? CustomInstructions = null);
+        // Parameterless constructor
+        public Order() { }
+
+        // Parameterized constructor
+        public Order(string orderId, string userId, string storageUnitId, OrderPeriod orderPeriod, OrderStatus? status, OrderInsurance? insurance, string? customInstructions)
+        {
+            OrderId = orderId;
+            UserId = userId;
+            StorageUnitId = storageUnitId;
+            OrderPeriod = orderPeriod;
+            Status = status;
+            Insurance = insurance;
+            CustomInstructions = customInstructions;
+        }
+    }
+}
