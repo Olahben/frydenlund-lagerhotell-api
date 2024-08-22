03d6ba0a358bcbc0b019c760c447643ce0fb58e2
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-22 14:30:42 +0200

    Create

---
+++ Lagerhotell.Shared/DbModels/CompanyUserDocument.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace LagerhotellAPI.Models.DbModels;
+
+public class CompanyUserDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string CompanyUserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Name { get; set; }
+    public int CompanyNumber { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public Address Address { get; set; }
+
+    public CompanyUserDocument(string companyUserId, string firstName, string lastName, string name, int companyNumber, string email, string phoneNumber, Address address)
+    {
+        CompanyUserId = companyUserId;
+        FirstName = firstName;
+        LastName = lastName;
+        Name = name;
+        CompanyNumber = companyNumber;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Address = address;
+    }
+}
---
+++ Lagerhotell.Shared/DomainModels/CompanyUser.cs
@@ -0,0 +1,3 @@
+﻿namespace LagerhotellAPI.Models.DomainModels;
+
+public record CompanyUser(string CompanyUserId, string FirstName, string LastName, string Name, int CompanyNumber, string Email, string PhoneNumber, Address Address);
