1f36f8d82fcc60076697efcdcea48b5666670e2b
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-23 13:41:04 +0200

    Create

---
+++ LagerhotellAPI/Services/CompanyUsers/ICompanyUserService.cs
@@ -0,0 +1,12 @@
+﻿using LagerhotellAPI.Models.DbModels;
+namespace LagerhotellAPI.Services;
+public interface ICompanyUserService
+{
+
+    Task<CompanyUserDocument> GetCompanyUserAsync(string id);
+    Task<List<CompanyUserDocument>> GetCompanyUsersAsync(int? take, int? skip);
+    Task<CompanyUser> CreateCompanyUserAsync(CompanyUser companyUser);
+    Task UpdateCompanyUserAsync(string id, CompanyUser companyUser);
+    Task DeleteCompanyUserAsync(string id);
+
+}
