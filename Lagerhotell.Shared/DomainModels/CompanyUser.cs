c939342f0c93c198bf828af932e2dd3648fbfc04
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-08-29 10:44:59 +0200

    Add password field

--- Lagerhotell.Shared/DomainModels/CompanyUser.cs
+++ Lagerhotell.Shared/DomainModels/CompanyUser.cs
@@ -1,3 +1,33 @@
 ﻿namespace LagerhotellAPI.Models.DomainModels;
 
-public record CompanyUser(string CompanyUserId, string FirstName, string LastName, string Name, int CompanyNumber, string Email, string PhoneNumber, Address Address);
+public class CompanyUser
+{
+    public string CompanyUserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Name { get; set; }
+    public int CompanyNumber { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    public Address Address { get; set; }
+
+    public CompanyUser(string companyUserId, string firstName, string lastName, string name, int companyNumber, string email, string phoneNumber, Address address, string password)
+    {
+        CompanyUserId = companyUserId;
+        FirstName = firstName;
+        LastName = lastName;
+        Name = name;
+        CompanyNumber = companyNumber;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        Password = password;
+    }
+
+    public CompanyUser()
+    {
+        // empty ctor
+        Address = new Address();
+    }
+};
