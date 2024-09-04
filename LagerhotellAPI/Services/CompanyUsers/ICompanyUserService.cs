2d6049735d4d7999b03dd8afb989172065c07abd
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-09-04 11:16:03 +0200

    Add method "GetCompanyUserByPhoneNumber"

--- LagerhotellAPI/Services/CompanyUsers/ICompanyUserService.cs
+++ LagerhotellAPI/Services/CompanyUsers/ICompanyUserService.cs
@@ -4,6 +4,7 @@ public interface ICompanyUserService
 
     Task<CompanyUser> GetCompanyUserAsync(string id);
     Task<List<CompanyUser>> GetCompanyUsersAsync(int? take, int? skip);
+    Task<CompanyUser> GetCompanyUserByPhoneNumber(string phoneNumber);
     Task<(string, string)> CreateCompanyUserAsync(CompanyUser companyUser);
     Task UpdateCompanyUserAsync(string id, CompanyUser companyUser);
     Task DeleteCompanyUserAsync(string id);
