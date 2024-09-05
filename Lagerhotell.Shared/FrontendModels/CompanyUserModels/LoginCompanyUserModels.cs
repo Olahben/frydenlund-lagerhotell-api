0bf4c95ab91e74a0dac9c92a83b2507c20d3dfa3
Ola Hagen Bendixen <ohagenbendixen@gmail.com>
2024-09-05 08:42:03 +0200

    Create models

---
+++ Lagerhotell.Shared/FrontendModels/CompanyUserModels/LoginCompanyUserModels.cs
@@ -0,0 +1,4 @@
+﻿namespace LagerhotellAPI.Models.FrontendModels;
+
+public record LoginCompanyUserByEmailRequest(string Email, string Password);
+public record LoginCompanyUserByEmailResponse(string AccessToken, string UserId);
