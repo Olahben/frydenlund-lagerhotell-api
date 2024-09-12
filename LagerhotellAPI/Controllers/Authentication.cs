d5d03760f8b6baf84fbe03320d2fd05d33a8535c
Ola Hagen Bendixen <olaben@afk.no>
2024-09-12 13:55:15 +0200

    Create controller

---
+++ LagerhotellAPI/Controllers/Authentication.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LagerhotellAPI.Models.CustomExceptionModels;
+
+namespace LagerhotellAPI.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly AccountManagementService _accountManagementService;
+
+    public AuthenticationController(AccountManagementService accountManagementService)
+    {
+        _accountManagementService = accountManagementService;
+    }
+
+    [HttpPost]
+    [Route("start-email-verification")]
+    public async Task<IActionResult> StartEmailVerification([FromBody] StartEmailVerificationRequest request)
+    {
+        await _accountManagementService.StartEmailVerification(request.Email);
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("verify-email-verification-code")]
+    public async Task<IActionResult> VerifyEmailVerificationCode([FromBody] VerifyEmailVerificationCodeRequest request)
+    {
+        try
+        {
+            var result = await _accountManagementService.VerifyEmailVerificationCode(request.Email, request.Code);
+        } catch (KeyNotFoundException)
+        {
+            return NotFound();
+        } catch (InvalidOperationException)
+        {
+            return StatusCode(410);
+        } catch (InvalidVerificationCodeException)
+        {
+            return BadRequest();
+        }
+        return Ok();
+    }
+}
