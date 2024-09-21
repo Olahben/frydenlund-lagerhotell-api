1c5405184947b215869e15a9177323c06b8674a3
Ola Hagen Bendixen <olaben@afk.no>
2024-09-21 15:35:36 +0200

    Add "UserAuth0Id" field

---
+++ LagerhotellAPI/Controllers/Auth0Users.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LagerhotellAPI.Controllers;
+
+[Route("auth0-users")]
+[ApiController]
+public class Auth0UsersController : ControllerBase
+{
+
+}
