4af64083fcf3c917c06611673cfbc49423cf8704
Ola Hagen Bendixen <olaben@afk.no>
2024-09-21 17:51:52 +0200

    Create controller, make endpoint for retrieving a user

--- LagerhotellAPI/Controllers/Auth0Users.cs
+++ LagerhotellAPI/Controllers/Auth0Users.cs
@@ -7,5 +7,40 @@ namespace LagerhotellAPI.Controllers;
 [ApiController]
 public class Auth0UsersController : ControllerBase
 {
+    private readonly Auth0UserService _auth0UserService;
+    public Auth0UsersController(Auth0UserService auth0UserService)
+    {
+        _auth0UserService = auth0UserService;
+    }
 
+    [HttpGet]
+    [Route("get-user")]
+    public async Task<IActionResult> GetUser([FromQuery] string auth0UserId)
+    {
+        try
+        {
+            var user = await _auth0UserService.GetCompleteUser(auth0UserId);
+            return Ok(user);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (BadRequestException e)
+        {
+            return BadRequest(e.Message);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            return Unauthorized(e.Message);
+        }
+        catch (TooManyRequestsException e)
+        {
+            return StatusCode(StatusCodes.Status429TooManyRequests, e.Message);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
 }
