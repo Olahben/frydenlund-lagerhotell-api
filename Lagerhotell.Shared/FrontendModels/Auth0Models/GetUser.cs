210b8f23c147316f8b7cf256eaccebb576c63548
Ola Hagen Bendixen <olaben@afk.no>
2024-09-26 09:00:49 +0200

    Create request response models

---
+++ Lagerhotell.Shared/FrontendModels/Auth0Models/GetUser.cs
@@ -0,0 +1,10 @@
+﻿using LagerhotellAPI.Models.DbModels.Auth0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerhotellAPI.Models.FrontendModels;
+
+public record GetAuth0UserResponse(UserAuth0 user);
---
+++ Lagerhotell.Shared/FrontendModels/Auth0Models/SignupUser.cs
@@ -0,0 +1,10 @@
+﻿using LagerhotellAPI.Models.DbModels.Auth0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerhotellAPI.Models.FrontendModels;
+
+public record SignupUserAuth0Request(UserAuth0 user);
