f0263767428b5dcff746249299e84c6a3c295dce
Ola Hagen Bendixen <olaben@afk.no>
2024-10-01 21:14:15 +0200

    Create models

---
+++ Lagerhotell.Shared/FrontendModels/Auth0Models/ExchangeCodeForTokens.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerhotellAPI.Models.FrontendModels;
+
+public record ExchangeCodeForTokensRequest(string Code);
+public record ExchangeCodeForTokensResponse(string AccessToken);
