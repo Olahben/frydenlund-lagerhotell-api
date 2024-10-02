29f2e17bdc6f955c71b67cd477c45b6de4ef7aef
Ola Hagen Bendixen <olaben@afk.no>
2024-10-02 20:13:54 +0200

    create

---
+++ Lagerhotell.Shared/DbModels/Auth0/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerhotellAPI.Models.DbModels.Auth0;
+
+public record RefreshTokenDocument(string RefreshToken, string UserAuth0Id);
