e4f5e54eb1fa90ccde12899d1a65344d2d4048b8
Ola Hagen Bendixen <olaben@afk.no>
2024-10-02 20:14:05 +0200

    Create repository

---
+++ LagerhotellAPI/Repositories/RefreshTokens.cs
@@ -0,0 +1,33 @@
+﻿global using MongoDB.Driver;
+global using LagerhotellAPI.Models.DbModels.Auth0;
+global using LagerhotellAPI.Models;
+using LagerhotellAPI.Models.DbModels;
+namespace LagerhotellAPI.Repositories;
+
+public class RefreshTokens
+{
+    private readonly IMongoCollection<Models.DbModels.Auth0.RefreshTokenDocument> _refreshTokens;
+
+    public RefreshTokens(MongoDbSettings settings)
+    {
+        var client = new MongoClient(settings.ConnectionString);
+        var database = client.GetDatabase("Lagerhotell");
+        _refreshTokens = database.GetCollection<Models.DbModels.Auth0.RefreshTokenDocument>("RefreshTokens");
+    }
+
+    public async Task<Models.DbModels.Auth0.RefreshTokenDocument?> GetRefreshToken(string refreshToken)
+    {
+        Models.DbModels.Auth0.RefreshTokenDocument? refreshTokenDocument = await _refreshTokens.Find(refreshTokenDocument => refreshTokenDocument.RefreshToken == refreshToken).FirstOrDefaultAsync();
+        return refreshTokenDocument;
+    }
+
+    public async Task CreateRefreshToken(Models.DbModels.Auth0.RefreshTokenDocument refreshTokenDocument)
+    {
+        await _refreshTokens.InsertOneAsync(refreshTokenDocument);
+    }
+
+    public async Task DeleteRefreshToken(string refreshToken)
+    {
+        await _refreshTokens.DeleteOneAsync(refreshTokenDocument => refreshTokenDocument.RefreshToken == refreshToken);
+    }
+}
