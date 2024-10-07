7e97c30ac65cf6efd0f738d4a09ce931dc71f19c
Ola Hagen Bendixen <olaben@afk.no>
2024-10-07 15:03:04 +0200

    Create

---
+++ Lagerhotell.Shared/DomainModels/Other/UserAndCompanyUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagerhotellAPI.Models.DomainModels;
+
+public class UserAndCompanyUser
+{
+    public UserAndCompanyUser(User? user, CompanyUser? companyUser)
+    {
+        CompanyUser = companyUser;
+        User = user;
+    }
+    public CompanyUser? CompanyUser { get; set; }
+    public User? User { get; set; }
+}
