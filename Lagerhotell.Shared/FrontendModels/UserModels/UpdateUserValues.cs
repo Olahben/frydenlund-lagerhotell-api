a71fe8fb2ee8457e7b97cecc0a69fba077a1e30b
Ola Hagen Bendixen <olaben@afk.no>
2024-10-10 09:52:00 +0200

    Add request model

--- Lagerhotell.Shared/FrontendModels/UserModels/UpdateUserValues.cs
+++ Lagerhotell.Shared/FrontendModels/UserModels/UpdateUserValues.cs
@@ -1,5 +1,22 @@
 ﻿namespace LagerhotellAPI.Models.FrontendModels;
 
+public class UpdateUserValuesRequest2
+{
+    public required string UserId { get; set; }
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required string PhoneNumber { get; set; }
+
+    public required string BirthDate { get; set; }
+    public required string Password { get; set; }
+    public required Address Address { get; set; }
+    public required bool IsAdministrator { get; set; }
+    public required string Email { get; set; }
+    public required bool IsEmailVerified { get; set; }
+}
+
 public class UpdateUserValuesRequest
 {
     public required string FirstName { get; set; }
@@ -13,7 +30,6 @@ public class UpdateUserValuesRequest
     public required Address Address { get; set; }
     public required bool IsAdministrator { get; set; }
     public required string Email { get; set; }
-    public required bool IsEmailVerified { get; set; }
 }
 
 public class UpdateUserValuesResponse
