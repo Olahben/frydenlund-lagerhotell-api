b987fc4d080e818464e59cee87c96c426d56f7c2
Ola Hagen Bendixen <olaben@afk.no>
2024-10-19 18:46:24 +0200

    Add rounded area and volume field

--- Lagerhotell.Shared/FrontendModels/Custom/StorageUnitSize.cs
+++ Lagerhotell.Shared/FrontendModels/Custom/StorageUnitSize.cs
@@ -3,7 +3,9 @@
 public struct StorageUnitSize
 {
     public double? Area { get; set; }
+    public decimal RoundedArea { get; set; }
     public double? Volume { get; set; }
+    public decimal RoundedVolume { get; set; }
     public Money Price { get; set; }
     public bool Temperated { get; set; }
     public string? InfoText { get; set; }
